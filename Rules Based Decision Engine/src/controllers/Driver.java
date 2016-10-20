c26777112043d2e5cb9a52d4b8e87097931abf68
MikeMosc <michaelmosc@gmail.com>
2016-10-20 16:07:28 -0400

    Added the FireRules Class (Not fully funct.) #39

--- Rules Based Decision Engine/src/controllers/Driver.java
+++ Rules Based Decision Engine/src/controllers/Driver.java
@@ -17,6 +17,15 @@ public class Driver {
         InputReaderService read_svc = InputReaderService.getInstance();
         ObjectCollectionService collection_svc = ObjectCollectionService.getInstance();
         read_svc.readJSONFile(file);
+       // String file = "./src/temp.txt";
+       // TextFileService par_svc = TextFileService.getInstance();
+       // ObjectCollectionService collection_svc = ObjectCollectionService.getInstance();
+        
+       // par_svc.parseTextFile(file);
+        // System.out.println(collection_svc);
+        ObjectData od = new ObjectData("Temperature", "Double", 37.6 );
+
+        FireRules fr = new FireRules("avoid.drl", od);
     	
     	System.out.println(collection_svc);
     	
