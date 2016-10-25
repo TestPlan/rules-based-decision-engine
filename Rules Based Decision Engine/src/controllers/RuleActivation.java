1a1511881b314747b6c71a5212613e0ac3c37153
MikeMosc <michaelmosc@gmail.com>
2016-10-25 17:30:33 -0400

    fix merge conflict.

--- Rules Based Decision Engine/src/controllers/RuleActivation.java
+++ Rules Based Decision Engine/src/controllers/RuleActivation.java
@@ -77,7 +77,8 @@ public class RuleActivation
 
     public void addNonExistingFile()
     {
-        kfs.write(kfsFilePath, getRule());
+        kfs.write(kfsFilePath, getRule());  //kfsFilePath is the file path necessary to use the KieFileSystem.
+                                            //getRule() is a string that contains the exact contents of the drl for the kfs to read.
     }
 
     public KieSession buildKnowledgeSession(ObjectData objectData)
