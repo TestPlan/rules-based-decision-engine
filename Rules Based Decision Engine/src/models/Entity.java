262419f21f1ba52a52dcb478adb10e200ec2da90
Michael Crinite <mikecrinite@gmail.com>
2016-11-04 18:09:56 -0400

    Created Entity class

---
+++ Rules Based Decision Engine/src/models/Entity.java
@@ -0,0 +1,36 @@
+package models;
+
+import java.util.HashMap;
+
+/**
+ * An Entity is a collection of Data objects.
+ *
+ * An Entity represents a "thing" like, for example, a submarine.
+ * The submarine may have a temperature, latitude, and longitude.
+ * The HashMap field <code>map</code> is a collection of the above datapoints as Data objects.
+ *
+ * Created by michaelcrinite on 11/4/16.
+ */
+public class Entity
+{
+    private String name;
+    private HashMap<String, Data> map;
+
+    public Object getValue(String str)
+    {
+        str = str.trim().toUpperCase();
+        Data temp = null;
+        temp = map.get(str);
+
+        if(temp != null)
+        {
+            return temp.getData();
+        }else{
+            return null;
+        }
+    }
+
+    //TODO: Append maps
+
+    //TODO: Replace current map with new map
+}
