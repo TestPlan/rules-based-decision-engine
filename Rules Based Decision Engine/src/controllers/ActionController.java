260dae722d6a0ae46ecd0a370004bc1ced543891
mikecrinite <mikecrinite@gmail.com>
2016-11-17 20:16:18 -0500

    Created ActionController

---
+++ Rules Based Decision Engine/src/controllers/ActionController.java
@@ -0,0 +1,42 @@
+package controllers;
+
+import exceptions.ActionException;
+import models.Action;
+import services.ActionCollectionService;
+
+/**
+ * Controller for interfacing between the + New Action view and the Action model.
+ *
+ * @author Michael Crinite
+ * @version 11/17/16
+ */
+public class ActionController
+{
+    public static ActionController INSTANCE = null;
+
+    public ActionController() {}
+
+    public static ActionController getInstance()
+    {
+        if (INSTANCE == null)
+        {
+            INSTANCE = new ActionController();
+        }
+        return INSTANCE;
+    }
+
+    public void newAction(String actionName, String actionDescription)
+    {
+        Action action = new Action(actionDescription);
+        ActionCollectionService acr = ActionCollectionService.getInstance();
+
+        try
+        {
+            acr.insertActions(actionName, action);
+        }
+        catch(ActionException ae)
+        {
+            System.out.println(ae.getMessage());
+        }
+    }
+}
