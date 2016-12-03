87c3a402afa14e8dda3647e5e62be56e9e842055
mikecrinite <mikecrinite@gmail.com>
2016-12-03 18:07:50 -0500

    Added functionality to ActionController

--- Rules Based Decision Engine/src/controllers/ActionController.java
+++ Rules Based Decision Engine/src/controllers/ActionController.java
@@ -8,7 +8,7 @@ import java.util.Collection;
 import java.util.Collections;
 
 /**
- * Controller for interfacing between the + New Action view and the Action model.
+ * Controller for allowing the user to access Action objects without directly interacting with them.
  *
  * @author Michael Crinite
  * @version 11/17/16
@@ -16,10 +16,17 @@ import java.util.Collections;
 public class ActionController
 {
     public static ActionController INSTANCE;
-    ActionCollectionService acr = ActionCollectionService.getInstance();
+    ActionCollectionService acs = ActionCollectionService.getInstance();
 
+    /**
+     * Default class constructor for type ActionController
+     */
     public ActionController() {}
 
+    /**
+     * Returns the Class instance if it exists, creates one if it does not
+     * @return The single instance of the ActionController class
+     */
     public static ActionController getInstance()
     {
         if (INSTANCE == null)
@@ -29,20 +36,72 @@ public class ActionController
         return INSTANCE;
     }
 
+    /**
+     * Creates an Action and adds it to the Action Collection Service
+     *
+     * @param actionName Key to store Action in the Collection service with
+     * @param actionDescription String to declare in Action object creation
+     */
     public void newAction(String actionName, String actionDescription)
     {
         Action action = new Action(actionDescription);
 
-        acr.put(actionName, action);
+        acs.put(actionName, action);
     }
 
+    /**
+     * Retrieve all existing Actions from the Collection service
+     * @return A String[] of all Actions in the collection
+     */
     public String[] retrieveActions()
     {
-        return acr.getActions();
+        return acs.getActions();
     }
 
+    /**
+     * Retrieve a single Action from the Collection service
+     * @param key Action key to search for
+     * @return Action stored by the given key
+     */
     public Action getAction(String key)
     {
-        return acr.get(key);
+        return acs.get(key);
+    }
+
+    /**
+     * Remove all Actions from the collection
+     */
+    public void clearActions()
+    {
+        acs.clear();
+    }
+
+    /**
+     * Removes a single Action from the collection
+     * @param key Action key to search for
+     * @return The removed action
+     */
+    public Action removeAction(String key)
+    {
+        return acs.remove(key);
+    }
+
+    /**
+     * Checks that an Action exists matching either the given key or given description
+     * @param Action String to search for. If the string matches any in an Action the collection, the Action exists
+     * @return True if an Action exists with the specified Action String
+     */
+    public boolean exists(String Action)
+    {
+        Action temp;
+        for(String s : retrieveActions())
+        {
+            temp = getAction(s);
+            if(temp.getAction().equalsIgnoreCase(Action.trim()))
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
